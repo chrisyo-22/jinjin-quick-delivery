31e3c0846641068f6ad3b7312709b1ba2d873113
Chris <chrisqiu22@gmail.com>
2026-01-03 13:42:06 -0500

    added set meal apis for CRUD operations

---
+++ jinjin-server/src/main/java/com/jinjin/controller/SetmealController.java
@@ -0,0 +1,107 @@
+package com.jinjin.controller;
+
+import com.jinjin.dto.SetmealDTO;
+import com.jinjin.dto.SetmealPageQueryDTO;
+import com.jinjin.result.PageResult;
+import com.jinjin.result.Result;
+import com.jinjin.service.SetmealService;
+import com.jinjin.vo.SetmealVO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 套餐管理
+ */
+@RestController
+@RequestMapping("/admin/setmeal")
+@Tag(name = "套餐相关接口")
+@Slf4j
+public class SetmealController {
+
+    @Autowired
+    private SetmealService setmealService;
+
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     * @return
+     */
+    @PostMapping
+    @Operation(summary = "新增套餐")
+    public Result save(@RequestBody SetmealDTO setmealDTO) {
+        log.info("新增套餐：{}", setmealDTO);
+        setmealService.saveWithDish(setmealDTO);
+        return Result.success();
+    }
+
+    /**
+     * 分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @Operation(summary = "分页查询")
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO) {
+        log.info("分页查询套餐：{}", setmealPageQueryDTO);
+        PageResult pageResult = setmealService.pageQuery(setmealPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 批量删除套餐
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    @Operation(summary = "批量删除套餐")
+    public Result delete(@RequestParam List<Long> ids) {
+        log.info("批量删除套餐：{}", ids);
+        setmealService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    /**
+     * 根据id查询套餐，用于修改页面回显数据
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    @Operation(summary = "根据id查询套餐")
+    public Result<SetmealVO> getById(@PathVariable Long id) {
+        log.info("根据id查询套餐：{}", id);
+        SetmealVO setmealVO = setmealService.getByIdWithDish(id);
+        return Result.success(setmealVO);
+    }
+
+    /**
+     * 修改套餐
+     * @param setmealDTO
+     * @return
+     */
+    @PutMapping
+    @Operation(summary = "修改套餐")
+    public Result update(@RequestBody SetmealDTO setmealDTO) {
+        log.info("修改套餐：{}", setmealDTO);
+        setmealService.update(setmealDTO);
+        return Result.success();
+    }
+
+    /**
+     * 套餐起售停售
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @Operation(summary = "套餐起售停售")
+    public Result startOrStop(@PathVariable Integer status, Long id) {
+        log.info("套餐起售停售：{}, {}", status, id);
+        setmealService.startOrStop(status, id);
+        return Result.success();
+    }
+}
--- jinjin-server/src/main/java/com/jinjin/mapper/SetmealMapper.java
+++ jinjin-server/src/main/java/com/jinjin/mapper/SetmealMapper.java
@@ -1,8 +1,12 @@
 package com.jinjin.mapper;
 
+import com.github.pagehelper.Page;
 import com.jinjin.anno.AutoFill;
+import com.jinjin.dto.SetmealPageQueryDTO;
 import com.jinjin.entity.Setmeal;
 import com.jinjin.enumeration.OperationType;
+import com.jinjin.vo.SetmealVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -13,11 +17,36 @@ public interface SetmealMapper {
 
     /**
      * 根据分类id查询套餐的数量
-     * @param id
+     * @param categoryId
      * @return
      */
     @Select("select count(id) from setmeal where category_id = #{categoryId}")
-    Integer countByCategoryId(Long id);
+    Integer countByCategoryId(Long categoryId);
+
+    @AutoFill(OperationType.INSERT)
+    void insert(Setmeal setmeal);
+
+    /**
+     * 分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    Page<SetmealVO> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
+
+    /**
+     * 根据id查询套餐
+     * @param id
+     * @return
+     */
+    @Select("select * from setmeal where id = #{id}")
+    Setmeal getById(Long id);
+
+    /**
+     * 根据id删除套餐
+     * @param id
+     */
+    @Delete("delete from setmeal where id = #{id}")
+    void deleteById(Long id);
 
     @AutoFill(OperationType.UPDATE)
     void update(Setmeal setmeal);
